94165faf508dd562f09ffe6824576debc9fc8921
KarthikaChinnasamy <karthikakcs21@gmail.com>
2016-09-11 14:27:38 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s;
+scanf("%c",s);
+if(s=='a'||s=='e'||s=='i'||s=='o'||s=='u')
+printf("vowel");
+else
+printf("consonants");
+getch();
+}
